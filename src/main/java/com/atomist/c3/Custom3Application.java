dbd3f1997a9a416184bb4c6fc57a8939af3f30b9
Rod Johnson <johnsonroda@gmail.com>
2018-02-17 23:02:50 +1100

    Initial commit from Atomist

---
+++ src/main/java/com/atomist/c3/Custom3Application.java
@@ -0,0 +1,12 @@
+package com.atomist.c3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Custom3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Custom3Application.class, args);
+	}
+}
